62cfec789c63959359ac386fb63cc1fb68511015
David Baker <drbaker2@gmail.com>
2017-06-30 17:43:18 -0400

    experimenting with whoa in js files

---
+++ components/Sample.js
@@ -0,0 +1,30 @@
+import remarkWhoa from 'remark-whoa';
+import unified from 'unified';
+import remarkParse from 'remark-parse';
+import * as marky from 'marky';
+
+import Whoa from '../whoa-components';
+
+const whoaParse = unified().use(remarkParse).use(remarkWhoa).parse;
+
+const Sample = () => {
+  marky.mark('whoa Parse')
+  const content = whoaParse(
+    `
+# Writing with \`.whoa\` inside of javascript files
+My expectation that it would be expensive to use [remark](https://github.com/wooorm/remark) to parse files during runtime was completely wrong. **Remark is very fast.**
+
+I profiled the toll that parsing a chunk of \`.whoa\`, which is not much more than just using [Woorm](https://github.com/wooorm)'s [unified module](https://unifiedjs.github.io/) and [remark-parse module](https://github.com/wooorm/remark/tree/master/packages/remark-parse)—
+\`unified().use(remarkParse).use(remarkWhoa).parse\`. I used [marky](https://github.com/nolanlawson/marky) to take care of the intricacies of some of the performance measurements. You can profile this page yourself in the Chrome DevTools Performance pane—I've left the User Timing measurement in there, but if you don't want to do that, here's a snapshot.
+
+![whoa-parse-profile](/static/blog/whoa-parse-profile.png) 
+
+So it shows parsing a few paragraphs of \`.whoa\` took 288.74 ms, but take note that this is with *10x CPU slowdown* on. 
+
+## Anyways, the whole thing gets prebuilt into html ahead of time, so it isn't really a concern.
+ `);
+  marky.stop('whoa Parse');
+  return <Whoa content={content} />;
+};
+
+export default Sample;
--- pages/sketches.js
+++ pages/sketches.js
@@ -3,55 +3,16 @@ import Link from 'next/link';
 import withRedux from 'next-redux-wrapper';
 import { initStore } from '../store';
 import Nav from '../components/Nav';
+import Sample from '../components/Sample';
+
+
 
 import PageWrapper from '../components/PageWrapper';
 
 const IndexPage = ({ url }) =>
   <PageWrapper title="🙃🐢">
     <Nav url={url} />
-    <main style={{ padding: '10px' }}>
-      <h2>
-        Hello!
-      </h2>
-      <p>
-        If you've gotten here, it's probably because I am applying for a job!
-      </p>
-      <p>
-        <label htmlFor="traditional">
-          Would you like to see a more traditional cover letter?
-        </label>
-        <input type="checkbox" id="traditional" />
-      </p>
-
-      <p>
-        You should check out the projects page. A lot of stuff on there is
-        unpolished personal projects, but still worth a look.
-      </p>
-
-      <p>
-        These days,{' '}
-        <strong>I am a frontend developer.</strong> I love making stuff, and I love the pace that comes with with writing software.
-      </p>
-
-      <p>
-        I used to (back in college) focus more on the hardware side of things but have found software to be much more accessible now that I don't have 24/7 access to lab full of all the discrete electronic components I might ever need 😢.
-      </p>
-
-      <p>
-        Do I know everything? <strong>Certainly not!</strong> There is sooooo much to learn about in our industry and the world, at times it can be overwhelming. I do however, learn quickly and I am eager to fill gaps in my knowledge.
-      </p>
-
-      <p>I am drawn to good ol' clean design that serves a purpose.</p>
-
-      <p>...</p>
-    </main>
-    <style jsx global>
-      {`
-        body {
-          --color-border: darkslategray;
-        }
-      `}
-    </style>
+    <Sample></Sample>
   </PageWrapper>;
 
 IndexPage.getInitialProps = async ({ store, isServer }) => {
