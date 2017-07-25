96052a6aee5b0309846e1e48c8c9bc7775ebd54d
David Baker <drbaker2@gmail.com>
2017-07-25 10:42:24 -0500

    custom server (for dev)

---
+++ server.js
@@ -0,0 +1,34 @@
+/**
+ * Server is only for development.
+ */
+
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+
+const app = next({ dev: true });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    // Be sure to pass `true` as the second argument to `url.parse`.
+    // This tells it to parse the query portion of the URL.
+    const parsedUrl = parse(req.url, true);
+    const { pathname, query } = parsedUrl;
+
+    if (pathname.match(/^\/blog\/.+/)) {
+      console.log('query', query);
+      console.log('pathname', pathname);
+      console.log(
+        'stringified',
+        JSON.stringify(pathname.match(/blog\/(.*)/)[1])
+      );
+      app.render(req, res, '/blog', { slug: pathname.match(/blog\/(.*)/)[1] });
+    } else {
+      handle(req, res, parsedUrl);
+    }
+  }).listen(3002, (err) => {
+    if (err) throw err;
+    console.log('>Ready on http://localhost:3002');
+  });
+});
